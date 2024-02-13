a86a1f0cb1ddc1216924dad8d8a1ebddf5aea1f9
sernamalala <sernaprograms@gmail.com>
2024-02-13 19:41:57 +0200

    Interface window and properties set

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -6,5 +6,16 @@ public class UserInterface extends JFrame {
 //constructor
     public UserInterface(){
         super("Weather App");
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        //size of GUI
+        setSize(450,650);
+        //center GUI
+        setLocationRelativeTo(null);
+        //makes it possible for us to set width and height as we want
+        setLayout(null);
+        //prevent resize
+        setResizable(false);
     }
 }
