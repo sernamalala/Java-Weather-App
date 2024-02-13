db4a12e7484a433aa8acf07e9c1fc14ec20fff02
sernamalala <sernaprograms@gmail.com>
2024-02-13 19:33:21 +0200

    Basic app setup and basic interface class

---
+++ src/UserInterface.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+//imagine JFrame class was there
+//inherit
+public class UserInterface extends JFrame {
+//constructor
+    public UserInterface(){
+        super("Weather App");
+    }
+}
