e8606ac3c50da047d543bd651e1646e63f80c09a
sernamalala <sernaprograms@gmail.com>
2024-02-13 19:52:26 +0200

    Added basic app launcher visibility of app in run method

---
+++ src/AppLauncher.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class AppLauncher {
+    //basically the main class
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+                new UserInterface().setVisible(true);
+            }
+        });
+    }
+}
