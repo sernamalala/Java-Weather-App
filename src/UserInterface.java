ffa35c07c243117136d5fc56e3fc3550d7752fca
sernamalala <sernaprograms@gmail.com>
2024-02-13 20:21:10 +0200

    Search button added

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,5 +1,9 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.File;
 
 //imagine JFrame class was there
 //inherit
@@ -35,5 +39,38 @@ public class UserInterface extends JFrame {
 
         //add searchBar so that its visible
         add(searchBar);
+
+        //search button
+        JButton searchButton = new JButton(loadImage("assets/search.png"));
+
+        //cursor pointer
+
+        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+
+        //set where the component will be located
+
+        searchButton.setBounds(375, 13, 47, 45);
+
+        //add search button
+        add(searchButton);
+
+    }
+
+    //use this to load images for the GUI components
+    private ImageIcon loadImage(String resourcePath){
+
+        try {
+            //read image from path given
+            BufferedImage image = ImageIO.read(new File(resourcePath));
+
+            //return image icon to be rendered
+            return new ImageIcon(image);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+            System.out.println("Error: "+e.getMessage());
+        }
+        System.out.println("Could not find the resource mentioned.");
+        return null;
     }
 }
